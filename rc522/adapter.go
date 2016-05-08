8f12e021ebab148dc452cea5e5b5e1f7fb0a0535
Sebastian Pozoga <sebastian@pozoga.eu>
2016-05-08 22:53:31 +0200

    golang bugfixes

--- rc522/adapter.go
+++ rc522/adapter.go
@@ -20,7 +20,7 @@ func NewRfidReader() (rfid.RfidReader, error) {
   if instance == nil {
     errCode, err := C.InitRC522RfidReader()
     if err != nil {
-      return err, nil
+      return nil, err
     }
     if errCode != 0 {
       return nil, fmt.Errorf("Init rc522 fail with code: ", errCode)
@@ -33,7 +33,7 @@ func NewRfidReader() (rfid.RfidReader, error) {
 func (r RfidReader) ReadId() (string, error) {
   response, err := C.ReadIdByRC522()
   if err != nil {
-    return err, nil
+    return "", err
   }
   if response.errorCode != C.NO_ERROR {
     return "", fmt.Errorf("Read rc522 fail with code: ", response.errorCode)
@@ -41,5 +41,5 @@ func (r RfidReader) ReadId() (string, error) {
   if response.status != C.NO_TAG_STATUS {
     return "", nil
   }
-  return C.GoString(&response.id), nil
+  return C.GoString(&response.id[0]), nil
 }
